6e1e37e56a009e5507473a4d4bf8032b3730338c
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-06-23 16:27:15 +1000

    added the http to https to fix the api in the vercel

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const apiEndPoint = "http://www.omdbapi.com/?i=tt3896198&apikey=91c05eb9&";
+const apiEndPoint =
+  process.env.NODE_ENV === "production"
+    ? "https://www.omdbapi.com/?i=tt3896198&apikey=91c05eb9&"
+    : "http://www.omdbapi.com/?i=tt3896198&apikey=91c05eb9&";
 
 export const fetchMovieInfo = async (title) => {
   const url = apiEndPoint + "t=" + title;
